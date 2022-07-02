4157c4f14cc8cd76b04cdfc28a08a859e4f94803
Júnior Veras <iramarjunhorveras@gmail.com>
2022-07-02 17:54:47 -0300

    UPDATE COR (FUNÇÃO)

--- RPG/asciiarte.h
+++ RPG/asciiarte.h
@@ -11,14 +11,42 @@
 #include <windows.h>
 
 
+void cor(num)
+{
+    int numero = num;
+
+
+    switch (numero)
+    {
+    case 1: //vermelho
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+        break;
+
+    case 2: //azul
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+        break;
+    case 3: //branco
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        break;
+    default: //default
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        break;
+    }
+
+    // cor(1); //vermelho
+    // cor(2); //azul
+    // cor(3); //branco
+}
+
+
 void ASCIIinimigo() //TEMPLATE PARA ASCII
 {
     
- SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12); printf("                         ~O");SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);printf("               o~\n"); //LEMBRAR DO \N PARA QUEBRAR LINHAS
- SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12); printf("                       /~()-{---");SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);printf("     ---}-()~/\n");
- SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12); printf("                        /~)");SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);printf("               (~.\n");
- SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12); printf("                        ~ ~");SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);printf("               ~ ~\n");
- SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+cor(1); printf("                         ~O");cor(2);printf("               o~\n"); //LEMBRAR DO \N PARA QUEBRAR LINHAS
+cor(1); printf("                       /~()-{---");cor(2);printf("     ---}-()~/\n");
+cor(1); printf("                        /~)");cor(2);printf("               (~.\n");
+cor(1); printf("                        ~ ~");cor(2);printf("               ~ ~\n");
+cor(3);
 } //FIM ASCII INIMIGO
 
 
