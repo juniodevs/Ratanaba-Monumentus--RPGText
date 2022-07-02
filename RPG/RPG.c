033eebe0e6e3bb17c09525b369a5ca67a45fc6c1
Júnior Veras <iramarjunhorveras@gmail.com>
2022-07-02 01:06:16 -0300

    Update (Muitas coisas principalmente a função Dialogo)

--- RPG/RPG.c
+++ RPG/RPG.c
@@ -15,7 +15,7 @@ struct personagem {
     int espada;
     int gold;
     int pocao;
-}player;
+}player; //STRUCT PARA CRIAÇÃO DE PERSONAGEM
 
 
 struct enemy {
@@ -23,8 +23,7 @@ struct enemy {
     int pv;
     int ataque;
     int defesa;
-
-}inimigo;
+}inimigo; //PRE DEFINIÇÃO DE INIMIGO
 
 // loja(struct personagem *player){
 //     printf("#########################");
@@ -32,7 +31,7 @@ struct enemy {
 //     printf("Voce tem %d gold#\n", player->gold);
 //     printf("Opcoes\n");
 //     printf("#1 - Aprimorar espada - 10c - o Aprimoramento aumenta sua espada em 1 de dano\n");
-// }
+// } //LOJA DE APRIMORAMENTO DE ITENS
 
 ataquemagico(struct personagem *player, struct enemy *enemy) {
     int dano = 0;
@@ -41,7 +40,7 @@ ataquemagico(struct personagem *player, struct enemy *enemy) {
     enemy->pv = enemy->pv - dano;
     printf("\n%s atacou %s e causou %d de dano\n", player->name, enemy->name, dano);
     player->ataquemagico--;
-}
+} //ATAQUE MAGICO
 
 receberataque(struct personagem *player, struct enemy *enemy) {
     int dano = 0;
@@ -53,30 +52,27 @@ receberataque(struct personagem *player, struct enemy *enemy) {
     else
     {
         player->pvjogador = player->pvjogador - dano;
-        delay(1000);
+        
         printf("\n%s recebeu um ataque de %s e causou %d de dano\n", player->name, enemy->name, dano);
-        delay(1000);
+        
     }
-
-}
+} //FUNÇÃO DE RECEBER ATAQUE
 
 verificacaodemorte(struct personagem *player, struct enemy *enemy) {
-
     int coinsareceber = 0;
-
     if (player->pvjogador <= 0) {
         printf("\n%s morreu\n", player->name);
         printf("#########################\n");
         printf("#Voce perdeu o jogo!#\n");
-        delay(1000);
+        
         printf("\n#########################");
-        delay(1000);
+        
         printf("3");
-        delay(1000);
+        
         printf("2");
-        delay(1000);
+        
         printf("1");
-        delay(1000);
+        
         system("cls");
         carregarjogo(&player, &inimigo);
     }
@@ -84,16 +80,14 @@ verificacaodemorte(struct personagem *player, struct enemy *enemy) {
         printf("\n%s morreu\n", enemy->name);
         printf("\n#########################");
         coinsareceber = (rand()%10) + 10;
-        delay(1000);
+        
         printf("#Voce recebeu %d de coins#\n", coinsareceber);
         player->gold = player->gold + coinsareceber;
         system("pause");
         system("cls");
     }
-}
-
+}//FUNÇÃO DE VERIFICAÇÃO DE MORTE (PERSONAGEM E INIMIGO)
 regeneracao(struct personagem *player){
-
     if (player->pocao <= 0)
     {
         printf("Voce nao tem pocao de vida\n");
@@ -106,7 +100,7 @@ regeneracao(struct personagem *player){
         printf("#########################");
         player->pocao--;
     }
-}
+} //FUNÇÃO DE REGENERAÇÃO DE PV (PERSONAGEM)
 
 salvarjogo(struct personagem *player, struct enemy *enemy) {
     FILE *save;
@@ -124,7 +118,7 @@ salvarjogo(struct personagem *player, struct enemy *enemy) {
     fprintf(save, "ataqueinimigo: %d\n", enemy->ataque);
     fprintf(save, "defesainimigo: %d\n", enemy->defesa);
     fclose(save);
-}
+} //FUNÇÃO DE SALVAR JOGO
 
 carregarjogo(struct personagem *player, struct enemy *enemy) {
     FILE *save;
@@ -143,24 +137,24 @@ carregarjogo(struct personagem *player, struct enemy *enemy) {
     fscanf(save, "defesainimigo: %d\n", &enemy->defesa);
     fclose(save);
     nivel(player, enemy);
-} // LOAD
+} //FUNÇÃO DE CARREGAR JOGO
 
 resetenemy(struct enemy *enemy) {
     enemy->pv = 7 + rand()%10;
     enemy->ataque = 3 + rand()%5;
     enemy->defesa = 2 + rand()%2;
-} // reseta a vida e ataque do inimigo
+} //FUNÇÃO DE RESET DE INIMIGO VIDA/ATAQUE/DEFESA
 
 resetvida(struct personagem *player) {
     player->pvjogador = 20;
     player->ataque = 8;
     player->espada = 1;
-} // reseta a vida do jogador
+} //FUNÇÃO DE RESET DE VIDA/ATAQUE DE PERSONAGEM DEPOIS DE BATALHAS
 
 delay(int ms) {
     clock_t start = clock();
     while (clock() < start + ms);
-} // timer para ações
+} //FUNÇÃO DE ATRASO (BETA)
 
 atacar(struct personagem *player, struct enemy *enemy) {
     int dano = 0;
@@ -169,7 +163,7 @@ atacar(struct personagem *player, struct enemy *enemy) {
     dano = (player->espada + player->ataque) - enemy->defesa;
     enemy->pv = enemy->pv - dano;
     printf("\n%s atacou %s e causou %d de dano\n", player->name, enemy->name, dano);
-    delay(1000);
+    
 } //função de ataque ao inimigo
 
 batalha(struct personagem *player, struct enemy *inimigo){
@@ -211,7 +205,8 @@ batalha(struct personagem *player, struct enemy *inimigo){
     }
     if (inimigo->pv>=0)
     {
-        receberataque(player, inimigo); //recebe o ataque do inimigo
+        receberataque(player, inimigo); //RECEBER ATAQUE DO INIMIGO
+        verificacaodemorte(player, inimigo);
     }
     }
     else
@@ -226,12 +221,11 @@ batalha(struct personagem *player, struct enemy *inimigo){
 
 void Dialogo(char text[], int seconds)
 {   
-    system("cls");
     int x; double y;
-    // Delay precisa ser em milisegundos
+    // DELAY EM MILISSEGUNDOS
     sleep(seconds);
     
-        // Mostrar o texto
+        // PRINTAR O TEXTO DE FORMA LENTA
         for(x=0; text[x]!=NULL; x++)
         {
         printf("%c",text[x]);
@@ -240,21 +234,57 @@ void Dialogo(char text[], int seconds)
         }
         }
     
-    // SÃ³ continuar quando Enter for pressionado
+    // LIMPAR A TELA APOS O TEXTO UTILIZANDO O ENTER
+}
+
+void clear()
+{
     printf("\n\n[Pressione Enter para continuar]");
     char enter = 0;
-    while (enter != '\r' && enter != '\n') //verificação de ENTER
+    while (enter != '\r' && enter != '\n') // VERIFICAÇÃO DO ENTER
+    {
+        enter = getchar();
+    }
+    {
+        enter = getchar();
+    }
+    system("cls");
+}
+
+void DialogocomClear(char text[], int seconds)
+{   
+    system("cls");
+    int x; double y;
+    // DELAY EM MILISSEGUNDOS
+    sleep(seconds);
+    
+        // PRINTAR O TEXTO DE FORMA LENTA
+        for(x=0; text[x]!=NULL; x++)
+        {
+        printf("%c",text[x]);
+        for(y=0; y<=8888888; y++)
+        {
+        }
+        }
+    
+    // LIMPAR A TELA APOS O TEXTO UTILIZANDO O ENTER
+    printf("\n\n[Pressione Enter para continuar]");
+    char enter = 0;
+    while (enter != '\r' && enter != '\n') // VERIFICAÇÃO DO ENTER
+    {
+        enter = getchar();
+    }
     {
         enter = getchar();
     }
     
-    // Limpar console
+    // LIMPAR A TELA APOS O TEXTO UTILIZANDO O ENTER
     system("cls");
 }
 
-void arrowHere(int realPosition, int arrowPosition)
+void localdaseta(int realPosition, int posicaoDaTecla)
 {
-    if (realPosition == arrowPosition) {
+    if (realPosition == posicaoDaTecla) {
         printf("\t\t\t=> ");
     }
     else {
@@ -265,30 +295,30 @@ void arrowHere(int realPosition, int arrowPosition)
 void menu(struct personagem *player, struct enemy *inimigo)
 {
     system("cls");
-      int posicaodatecla = 1, keyPressed = 0;
+      int posicaodatecla = 1, KeyDown = 0;
     
       #define MAX 4
       #define MIN 1
     
-      while (keyPressed != 13)
+      while (KeyDown != 13)
       {
 
         system("cls");
 
-            // Mostrando menu
+            // MOSTRAR O MENU
             printf("\t\t\t------[MENU]------     ^     ________\n");
-            arrowHere(1, posicaodatecla); printf("NOVO JOGO          | |   |        |\n"); 
-            arrowHere(2, posicaodatecla); printf("CARREGAR JOGO      | |   |        | \n");
-            arrowHere(3, posicaodatecla); printf("CRÉDITOS          | |   |        |\n");
-            arrowHere(4, posicaodatecla); printf("SAIR               | |    \\      /\n");
-            arrowHere(5, posicaodatecla); printf("                 `--8--'   \\_  _/\n");
+            localdaseta(1, posicaodatecla); printf("NOVO JOGO          | |   |        |\n"); 
+            localdaseta(2, posicaodatecla); printf("CARREGAR JOGO      | |   |        | \n");
+            localdaseta(3, posicaodatecla); printf("CRÉDITOS          | |   |        |\n");
+            localdaseta(4, posicaodatecla); printf("SAIR               | |    \\      /\n");
+            localdaseta(5, posicaodatecla); printf("                 `--8--'   \\_  _/\n");
             printf("\t\t\t------------------     0        vv\n");
 
-            keyPressed = getch(); //RECEBER ENTER
+            KeyDown = getch(); //RECEBER ENTER
             
-            if (keyPressed == 80 && posicaodatecla != MAX) {
+            if (KeyDown == 80 && posicaodatecla != MAX) {
             posicaodatecla++;
-            } else if (keyPressed == 72 && posicaodatecla != MIN) {
+            } else if (KeyDown == 72 && posicaodatecla != MIN) {
             posicaodatecla--;
             } else {
             posicaodatecla = posicaodatecla;
@@ -298,21 +328,21 @@ void menu(struct personagem *player, struct enemy *inimigo)
     switch (posicaodatecla) {
         case 1:
             system("cls");
-            Dialogo("Um novo jogo esta sendo criado!", 0);
+            DialogocomClear("Um novo jogo esta sendo criado!\n", 0);
             resetvida(player);
             resetenemy(inimigo);
             nivel(player, inimigo);
             break;
         case 2:
             system("cls");
-            Dialogo("O seu jogo salvo esta sendo carregado!", 0);
+            DialogocomClear("Carregando jogo...", 0);
             carregarjogo(player, inimigo);
             nivel(player, inimigo);
             break;
         case 3:
             system("cls");
-            Dialogo("Obrigado por jogar!", 0);
-            Dialogo("Historia por: Julio Cesar \nVisual: Sergio Mota \nProgramacao: Junior Veras", 0);
+            DialogocomClear("Obrigado por jogar!", 0);
+            DialogocomClear("Historia por: Julio Cesar \nVisual: Sergio Mota \nProgramacao: Junior Veras", 0);
             menu(player, inimigo);
             break;
         case 4:
@@ -326,21 +356,16 @@ switch (player->nivel)
 {
 case 1:
     resetvida(player);
-    printf("Provavelmente e a primeira vez que voce coloca seus pes nessas terras distantes.\n\n");
-    delay(1000);
-    printf("Como podemos lhe chamar?\n\n");
+    Dialogo("Provavelmente e a primeira vez que voce coloca seus pes nessas terras distantes.\n\n", 0);
+    Dialogo("Como podemos lhe chamar?\n");
     scanf("%s", player->name);
-    delay(1000);
+    
     system("cls");
-    printf("Bem-vindo %s\n\n", player->name);
-    delay(1000);
-    printf("Antes e comecar irei lhe dar alguns itens\n\n");
-    delay(1000);
-    printf("Voce recebeu uma espada.\n\n");
-    delay(1000);
-    printf("Voce recebeu 20 pontos de vida e 9 pontos de ataque com espada e 5 pontos de defesa.\n\n");
-    delay(1000);
-    system("pause");
+    Dialogo("Bem-vindo", 0); printf(" %s\n", player->name);
+    delay(2000);
+    Dialogo("Antes e comecar irei lhe dar alguns itens\nVoce recebeu uma espada.\nVoce recebeu 20 pontos de vida e 9 pontos de ataque com espada e 5 pontos de defesa.", 0);
+    delay(2000);
+    clear();
 
     player->nivel = 2;
     salvarjogo(player, enemy);
@@ -351,38 +376,38 @@ case 1:
 
 case 2:
     resetvida(player);
-    delay(1000);
+    
     printf("Seu nome e %s\n\n", player->name);
-    delay(1000);
+    
     printf("Voce mora na Vila de Ratanaba, um guerreiro bastante valente,\n");
     printf("e por este motivo o Chefe-rei chama voce e alguns outros guerreiros\n");
     printf("para tratar sobre alguns assuntos secretos\n\n");
-    delay(1000);
+    
     system("pause");
     system("cls");
-    delay(1000);
+    
     printf("Voz desconecida: %s\n\n", player->name);
-    delay(1000);
+    
     printf("Narrador: Voce ouve seu nome ser chamado de longe\n");
-    delay(1000);
+    
     printf("Ao olhar para tras voce ver Alex, um velho amigo de infancia\n\n");
-    delay(1000);
+    
     printf("%s: Alex, a quanto tempo meu velho e bom amigo\n\n", player->name);
-    delay(1000);
+    
     printf("Alex: Eh, faz bastante tempo que nao nos vemos\n\n");
-    delay(1000);
+    
     printf("Alex: O Rei-Chafe tambem o chamou aqui hoje?\n\n");
-    delay(1000);
+    
     printf("%s: Sim, passei a noite tentando enteder do que se tratava esses segredos\n\n", player->name);
-    delay(1000);
+    
     system("pause");
     system("cls");
     printf("Guerreiro Maior: Ei voces, o chefe esta aguardando voces\n\n");
-    delay(1000);
+    
     printf("Narrador: Os guerreiros seguiram ate o altar do chefe\n\n");
-    delay(1000);
+    
     printf("Guerreiro Maior: %s, Boa Sorte!\n\n", player->name);
-    delay(1000);
+    
     system("pause");
     system("cls");
 
@@ -394,32 +419,29 @@ case 2:
     break;
 
     case 3:
-    delay(1000);
+    
     printf("Chefe-Rei: GUERREIROS, eu chamei hoje voces aqui, os mais fortes e valentes dessa vila\n");
     printf("para fazermos uma grande descoberta, encontramos mapas perdidos de nossos acenstrais\n\n");
-    delay(5000);
     printf("Chefe-Rei: Voces iram partir esta noite, e aquele que conseguir entronctrar o tesouro\n");
     printf("sera muito bem recompensado\n\n");
-    delay(1000);
+    
     printf("Guerreiro Azul: Ja que e apenas uma exploracao, porque vossa exelencia chamou estes tantos de guerreiros\n");
     printf("Eu faria isto sozinho sem nenhuma preocupacao\n\n");
-    delay(5000);
     printf("Chefe-Rei: Silencio garoto, nao e algo que voce possa fazer sozinho\n");
     printf("infelizmente nos nao somos os unicos que sabemos deste tesouro\n");
     printf("nossos inimigos tambem estao a caminho, entao peco-lhes que tomem bastante cuidado\n\n");
-    delay(5000);
     system("pause");
     system("cls");
     printf("Guerreiro Vermelho: Okay meu Chefe, como sera separado as equipes?\n\n");
-    delay(1000);
+    
     printf("Chefe-Rei: voces serao separados em 3 grupos\n");
-    delay(1000);
+    
     printf("Grupo 1: Guerreiro Azul e Vermelho\n");
-    delay(1000);
+    
     printf("Grupo 2: Guerreiro Verde e Amarelo\n");
-    delay(1000);
+    
     printf("Grupo 3: Alex e %s\n\n", player->name);
-    delay(1000);
+    
     system("pause");
     player->nivel = 4;
     salvarjogo(player, enemy);
@@ -431,10 +453,9 @@ case 2:
     case 4:
 
     printf("### NAQUELA NOITE ###\n\n");
-    delay(1000);
+    
     printf("Alex: voce tem ideia do que estamos preste a presenciar?\n", player->name);
     printf("eu ouvi alguns boatos sobre uma maldicao neste tesouro.\n\n");
-    delay(5000);
     int alternativa;
     printf("Escolha uma das alternativas:\n\n");
     printf("1 - Maldicao? isso nao existe, deve ser apenas boatos\n");
@@ -444,34 +465,32 @@ case 2:
     if (alternativa == 1)
     {
         printf("%s: Maldicao? isso nao existe, deve ser apenas boatos\n\n", player->name);
-        delay(1000);
+        
         printf("Alex: Acho que voce nao deveria subestimar esse tipo de coisa\n\n");
     }
     else if (alternativa == 2)
     {
         printf("%s: Eu tambem andei pensando muito sobre isso, mas nao podemos fazer nada\n\n", player->name);
-        delay(1000);
+        
         printf("Alex: Voce acha que ainda da tempo de desistir?\n\n");
-        delay(1000);
+        
         printf("%s: Se fizermos isso, o chefe iria nos queimar em praca publica\n\n", player->name);
-        delay(1000);
+        
         printf("Alex: Voce tem razao\n\n");
     }
     else if (alternativa == 3)
     {
         printf("%s: Nos vamos conseguir, afinal nos somos os melhores de ratanaba\n\n", player->name);
-        delay(1000);
+        
         printf("Alex: Verdade, devem ser apenas boatos\n\n");
     }
     else
     {
         printf("%s: Nos vamos conseguir, afinal nos somos os melhores de ratanaba\n\n", player->name);
-        delay(1000);
+        
         printf("Alex: Verdade, devem ser apenas boatos\n\n");
     }
-    delay(5000);
     printf("\nNarrador: Os dois guerreiros chegaram na carroca a qual eles iriam ate o monumento encontrado\n\n");
-    delay(5000);
 
     system("pause");
     system("cls");
@@ -488,24 +507,18 @@ case 2:
     resetvida(&player);
     resetenemy(&enemy);
     printf("### NAQUELA MADRUGADA ###\n\n");
-    delay(1000);
+    
     printf("Narrador: A carroca estava em total silencio, nenhum homem naquele lugar\n");
     printf("era capaz de falar uma palavra se quer\n");
-    delay(5000);
     system("pause");
     system("cls");
     printf("### UM POUCO MAIS TARDE ###");
-    delay(1000);
     printf("\nNarrador: A carroca chegou ao monumento, parecia ser uma piramide\n");
     printf("no primeiro momento, todos ficaram encucados com aquilo, pois nunca tinham\n");
     printf("visto algo de tamanha magnitude\n\n");
-    delay(2000);
     printf("Desconhecido: EEEEEEI!, ALI ESTAO ELES!\n\n");
-    delay(1000);
     printf("Narrador: Todos olharam para tras, parecia ser um guerreiro, mas nao de sua mesma vila\n");
-    delay(1000);
     printf("O Guerreiro correu ate os grupos, ele parecia estar bastante hostil\n\n");
-    delay(2000);
     printf("%s estava no alvo do Inimigo\n\n", player->name);
 
     system("pause");
@@ -527,7 +540,7 @@ case 2:
 
     case 6:
     printf("### CONTINUACAO ###\n\n");
-    delay(1000);
+    
     system("pause");
     break;
 }
